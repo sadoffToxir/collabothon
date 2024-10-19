dbd14f819d0a0a28d9f7ee93993726d83154167a
sadoffToxir <toxa.saidoff@gmail.com>
2024-10-19 10:39:51 +0200

    init and config of project

---
+++ eslint.config.js
@@ -0,0 +1,58 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'react/react-in-jsx-scope': 'off', 
+      'indent': ['error', 2],
+      'no-multi-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'array-bracket-spacing': ['error', 'never'],
+      'computed-property-spacing': ['error', 'never'],
+      'no-extra-parens': 'error',
+      'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 1 }],
+      'quotes': ['error', 'single'],
+      'simple-import-sort/imports': [
+        'error',
+        {
+          'groups': [
+            // `react` first, `next` second, then packages starting with a character
+            ['^react$', '^next', '^[a-z]'],
+            // Packages starting with `@`
+            ['^@'],
+            // Packages starting with `~`
+            ['^~'],
+            // Imports starting with `../`
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+            // Imports starting with `./`
+            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+            // Style imports
+            ['^.+\\.s?css$'],
+            // Side effect imports
+            ['^\\u0000']
+          ]
+        }
+      ]
+    },
+  },
+)
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    fontFamily: {
+      generalsans: ['General Sans', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'dark-background': '#1c1c1c',
+        'accent-color': '#2a2a2a',
+        'primary-accent': '#f1c40f',
+        highlight: '#ffd700',
+        'subtle-accent': '#e0e0e0',
+        'primary-text': '#ffffff',
+        'secondary-text': '#d1d1d1',
+        'emphasis-text': '#f1c40f',
+      },
+    },
+  },
+  plugins: [],
+};
