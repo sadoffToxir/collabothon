3238668424814b9ba60efca8d1b5f9a6ba4e4966
Adilet Baimyrza <adiletbaimyrza@gmail.com>
2024-10-19 21:50:56 +0200

    add isolation to components

--- src/components/QuickAccess/QuickAccess.tsx
+++ src/components/QuickAccess/QuickAccess.tsx
@@ -1,28 +1,22 @@
+import { useState } from 'react';
 import { ReactSVG } from 'react-svg';
 
-import services from './services';
+import { initialServices } from './initialServices';
+import { ServiceType } from './ServiceType';
 
 import './QuickAccess.scss';
 
 const QuickAccess = () => {
+  const [services, setServices] = useState<ServiceType[]>(initialServices);
+
   return (
-    <div className="quickAccess">
-      {services.map((row, rowKey) => 
-        <div className="quickAccess__servicesRow" key={rowKey}>
-          {
-            row.map((service, index) =>
-              <div
-                key={index}
-                className="service"
-              >
-                <div className='service__icon'>
-                  <ReactSVG 
-                    style={{ fill: service.color }}
-                    src={service.icon} />
-                </div>
-                <p>{service.name}</p>
-              </div>
-            )}
+    <div className="quickAccess max-w-[500px]">
+      {services.map((service) => 
+        <div className="service">
+          <div className="service__icon">
+            <ReactSVG style={{ fill: service.color }} src={service.icon} />
+          </div>
+          <p>{service.name}</p>
         </div>
       )}
     </div>
