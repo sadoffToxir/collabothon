931ccd9155ef2c904adfea135fa4fe86a7f5ade5
sadoffToxir <toxa.saidoff@gmail.com>
2024-10-19 15:40:45 +0200

    layout

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
 
+<<<<<<< Updated upstream
+import './index.scss';
+=======
 import '@styles/global.scss'
 import './index.scss'
+>>>>>>> Stashed changes
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
