46db98fa86289650c77d2d5590a447938dbc7a14
Adilet Baimyrza <adiletbaimyrza@gmail.com>
2024-10-19 18:35:50 +0200

    add

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import { ConfigProvider } from 'antd';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Approvals from '@components/approvals/Approvals';
+import { Kittens } from '@components/Kittens/Kittens';
 import DefaultLayout from '@components/layout/DefaultLayout';
-import NewFeaturesWidget from '@components/newFeaturesWidget/NewFeaturesWidget'
+import NewFeaturesWidget from '@components/NewFeaturesWidget/NewFeaturesWidget';
 import QuickAccess from '@components/QuickAccess/QuickAccess';
 import { theme } from '@plugins/antDesign';
 
@@ -13,13 +14,25 @@ function App() {
   return (
     <>
       <ConfigProvider theme={theme}>
-        <div className="App">
-          <DefaultLayout>
-            <Approvals />
-            <QuickAccess />
-            <NewFeaturesWidget />
-          </DefaultLayout>
-        </div>
+        <Router>
+          <Routes>
+            <Route path="/approvals" element={<Approvals />} />
+            <Route path="/quick-access" element={<QuickAccess />} />
+            <Route path='/kittens' element={<Kittens />} />
+            <Route
+              path="/"
+              element={
+                <div className="App">
+                  <DefaultLayout>
+                    <Approvals />
+                    <QuickAccess />
+                    <NewFeaturesWidget />
+                  </DefaultLayout>
+                </div>
+              }
+            ></Route>
+          </Routes>
+        </Router>
       </ConfigProvider>
     </>
   );
